80bdc2ffbaf0837fc1477165e71c54399d8d83ec
Jamie Norman <jamiedeez@gmail.com>
2022-02-24 20:38:06 -0500

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var wheels = new Car();
+            wheels.Make = "Chevy";
+            wheels.Model = "Spark";
+            wheels.Year = 2015;
+
+            Console.WriteLine($"Your very first car is a {wheels.Make} { wheels.Model} {wheels.Year}!");
+
+        }
+    }
+}
